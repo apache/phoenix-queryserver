1731342ba249a7e7dcf62fb1d088eccb1380014c
Josh Elser <elserj@apache.org>
2020-06-18 12:53:27 -0400

    Remove the extra phoenixdb directory, update RAT

--- python-phoenixdb/dev-support/run-source-ratcheck.sh
+++ python-phoenixdb/dev-support/run-source-ratcheck.sh
@@ -18,12 +18,14 @@ WORK_DIR="$DEV_SUPPORT/work"
 
 mkdir -p "$WORK_DIR" "$ARTIFACTS_DIR"
 
+# Cache the RAT binary artifacts
 if [[ ! -f "$ARTIFACTS_DIR/$RAT_BINARY_NAME" ]]; then
   echo "$ARTIFACTS_DIR/$RAT_BINARY_NAME does not exist, downloading it"
   $DEV_SUPPORT/cache-apache-project-artifact.sh --working-dir "$WORK_DIR" --keys https://www.apache.org/dist/creadur/KEYS \
     "$ARTIFACTS_DIR/$RAT_BINARY_NAME" "$RAT_BINARY_MIRROR_NAME"
 fi
 
+# Extract the RAT binary artifacts
 if [[ ! -d "$ARTIFACTS_DIR/$RAT_BINARY_DIR" ]]; then
   echo "$ARTIFACTS_DIR/$RAT_BINARY_DIR does not exist, extracting $ARTIFACTS_DIR/$RAT_BINARY_NAME"
   tar xf $ARTIFACTS_DIR/$RAT_BINARY_NAME -C $ARTIFACTS_DIR
@@ -32,4 +34,11 @@ fi
 echo "RAT binary installation localized, running RAT check"
 
 # Run the RAT check, excluding pyc files
-java -jar "$ARTIFACTS_DIR/$RAT_BINARY_DIR/$RAT_JAR" -d "$DEV_SUPPORT/../phoenixdb" -E "$DEV_SUPPORT/rat-excludes.txt"
+for src in 'phoenixdb' 'ci' 'examples' 'doc'; do 
+  echo "Running RAT check over $src"
+  java -jar "$ARTIFACTS_DIR/$RAT_BINARY_DIR/$RAT_JAR" -d "$DEV_SUPPORT/../$src" -E "$DEV_SUPPORT/rat-excludes.txt"
+  if [[ $? -ne 0 ]]; then
+    echo "Failed RAT check over $src"
+    exit 1
+  fi
+done
