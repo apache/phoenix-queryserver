5d393ea6f46df4f759670f3fea0b96bee0c38743
Istvan Toth <stoty@apache.org>
2020-09-28 19:15:10 +0200

    PHOENIX-6162 Apply PHOENIX-5594 to the phoenix-queryserver repo

--- bin/queryserver.py
+++ bin/queryserver.py
@@ -149,6 +149,10 @@ if command == 'start':
         sys.stderr.write("daemon mode not supported on this platform{}".format(os.linesep))
         sys.exit(-1)
 
+    # get the current umask for the sub process
+    current_umask = os.umask(0)
+    os.umask(current_umask)
+
     # run in the background
     d = os.path.dirname(out_file_path)
     if not os.path.exists(d):
@@ -171,6 +175,10 @@ if command == 'start':
                     child.send_signal(signum)
                 sys.exit(0)
             signal.signal(signal.SIGTERM, handler)
+
+            def initsubproc():
+                # set the parent's umask
+                os.umask(current_umask)
 
             print('%s launching %s' % (datetime.datetime.now(), cmd))
             child = subprocess.Popen(cmd.split())
