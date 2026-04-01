bc4b26aa6df055daabd718c2f783481fa6ae79be
adunai <adunai@cloudera.com>
2026-04-01 11:34:08 +0200

    Revert "PHOENIX-7779: Python changes so that we can run the tests - .sh changes need to be removed before merging."
    This reverts commit dec732f0a9707db8b6e58655aa0d60a7776303f0.

--- phoenix-queryserver-it/src/it/bin/test_phoenixdb.sh
+++ phoenix-queryserver-it/src/it/bin/test_phoenixdb.sh
@@ -48,9 +48,7 @@ shift 5
 
 PY_ENV_PATH=$( mktemp -d )
 
-#virtualenv $PY_ENV_PATH
-python -m venv "$PY_ENV_PATH"
-python --version
+virtualenv $PY_ENV_PATH
 
 pushd ${PY_ENV_PATH}/bin
 
@@ -62,8 +60,7 @@ popd
 
 set -u
 echo "INSTALLING COMPONENTS"
-echo ${LOCAL_PY}
-pip3 install -e file:///${LOCAL_PY}/
+pip install -e file:///${LOCAL_PY}/
 
 export KRB5_CONFIG=$KRB5_CFG_FILE
 cat $KRB5_CONFIG
